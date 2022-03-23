053716d672884892d027b4cd7682477c86333568
aktivgo <aktivgo01062001@gmail.com>
2022-03-23 20:06:04 +0300

    Try paint snowfalls

---
+++ task/Line.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+
+namespace task
+{
+    public class Line
+    {
+        private Point a, b;
+        
+        public Line(Point a, Point b)
+        {
+            if (a.X <= b.X)
+            {
+                this.a = a;
+                this.b = b;
+            }
+            else
+            {
+                this.a = b;
+                this.b = a;
+            }
+        }
+
+        public bool IsPointOwned(Point p)
+        {
+            var k = (a.Y - b.Y) / (a.X - b.X);
+            var offset = a.Y - a.X * k;
+            return p.Y == k * p.X + offset;
+        }
+    }
+}
--- task/MainForm.cs
+++ task/MainForm.cs
@@ -15,18 +15,21 @@ namespace task
 
         private const int N = 500;
         private List<int> x, y, vX, vY, P;
+        private List<Line> barriers;
         
         public MainForm()
         {
             InitializeComponent();
             Graph = CreateGraphics();
             Graph.SmoothingMode = SmoothingMode.HighQuality;
-            MyPen = new Pen(Color.Black);
+            MyPen = new Pen(Color.Black, 2);
             MyBrush = new SolidBrush(Color.Black);
             Random = new Random();
 
             x = new List<int>();
             y = new List<int>();
+
+            barriers = new List<Line>();
         }
         
         private void button1_Click(object sender, EventArgs e)
@@ -37,33 +40,73 @@ namespace task
             PaintSnowfall();
         }
 
+        private void Clear()
+        {
+            x.Clear();
+            y.Clear();
+            barriers.Clear();
+            /*vX.Clear();
+            vY.Clear();*/
+        }
+
         private void PaintSnowfall()
         {
-            Graph.DrawRectangle(MyPen, 1, 1, 100, 100);
-            Graph.FillRectangle(MyBrush, 1, 1, 100, 100);
+            Clear();
             
+            var xMax = ClientSize.Width;
+            var yMax = ClientSize.Height;
+            
+            for (var i = 0; i < N; i++)
+            {
+                x.Add(Random.Next(xMax));
+                y.Add(Random.Next(200, 400));
+            }
+            
+            barriers.Add(new Line(new Point(0, yMax - 600), new Point(xMax, yMax - 600)));
+            Graph.DrawRectangle(MyPen, 0, yMax - 600, xMax, 600);
+
+            while(true)
+            {
+                for (var i = 0; i < N; i++)
+                {
+                    if (y[i] < yMax && !barriers[0].IsPointOwned(new Point(x[i], y[i])))
+                    {
+                        MyPen.Color = BackColor;
+                        Graph.DrawLine(MyPen, x[i], y[i], x[i] + 1, y[i] + 1);
+
+                        y[i] += 1;
+
+                        MyPen.Color = Color.Black;
+                        Graph.DrawLine(MyPen, x[i], y[i], x[i] + 1, y[i] + 1);
+                    }
+                    else
+                    {
+                        x[i] = Random.Next(xMax);
+                        y[i] = Random.Next(200, 400);
+                        
+                        MyPen.Color = Color.Black;
+                        Graph.DrawRectangle(MyPen, x[i], y[i], 1, 1);
+                    }
+                }
+            }
+        }
+
+        private bool IsBarrier(int x, int y)
+        {
             var bounds = Screen.GetBounds(new Point(0, 0));
             var bmp = new Bitmap(bounds.Width, bounds.Height);
             using (var g = Graphics.FromImage(bmp))
-                g.CopyFromScreen(2, 30, 0, 0, bmp.Size);
-            
-            var image = new Bitmap(bmp);
-            var pixelColor = image.GetPixel(0, 0);
-
-            MyPen.Color = pixelColor;
-            MyBrush.Color = pixelColor;
-            Graph.DrawRectangle(MyPen, 150, 150, 100, 100);
-            Graph.FillRectangle(MyBrush, 150, 150, 100, 100);
-        }
-        
-        Color GetPixelColor(Point point) {
-            Color color;
-            using (Bitmap bmp = new Bitmap(Width, Height)) {
-                DrawToBitmap(bmp, new Rectangle(new Point(), Size));
-                bmp.Save("test.png", System.Drawing.Imaging.ImageFormat.Png);
-                color = bmp.GetPixel(point.X, point.Y);
+            {
+                g.CopyFromScreen(4, 60, 0, 0, bmp.Size);
             }
-            return color;
+
+            var image = new Bitmap(bmp);
+            var pixelColor = image.GetPixel(x, y);
+            
+            bmp.Dispose();
+            image.Dispose();
+
+            return pixelColor != BackColor;
         }
 
         private void PaintPistonModel()
@@ -119,12 +162,10 @@ namespace task
                 for (var i = 0; i < N; i++)
                 {
                     MyPen.Color = Color.WhiteSmoke;
-                    MyBrush.Color = Color.WhiteSmoke;
-                    Graph.DrawRectangle(MyPen, x[i], y[i], 2, 2);
-                    Graph.FillRectangle(MyBrush, x[i], y[i], 2, 2);
+                    Graph.DrawRectangle(MyPen, x[i], y[i], 1, 1);
                     
-                    var dX = -20 + Random.Next() % 41;
-                    var dY = -10 + Random.Next() % 41;
+                    var dX = Random.Next(-20, 41);
+                    var dY = Random.Next(-10, 41);
 
                     if (x[i] + dX > 0 && x[i] + dX < xMax)
                     {
@@ -136,9 +177,7 @@ namespace task
                     }
                     
                     MyPen.Color = Color.Black;
-                    MyBrush.Color = Color.Black;
-                    Graph.DrawRectangle(MyPen, x[i], y[i], 2, 2);
-                    Graph.FillRectangle(MyBrush, x[i], y[i], 2, 2);
+                    Graph.DrawRectangle(MyPen, x[i], y[i], 1, 1);
                 }
             }
         }
